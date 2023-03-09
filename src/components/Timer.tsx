09ed01e702f89aa540d5b84bdbbb17e72758f3f2
AlGuron <blizgur8@gmail.com>
2023-03-09 21:30:56 +0200

    fix: findTimeZone 1 time

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -9,15 +9,15 @@ export const Timer: React.FC<Props> = ({ cityCountry }) => {
     const styles: React.CSSProperties = {
         backgroundColor: "lightblue",
         fontSize: "2em"
-    };    
+    };
     const [time, setTime] = React.useState(new Date());
-    const myTimeZone = { timeZone: findTimeZone(cityCountry) };
+    const [myTimeZone, setTimeZone] = React.useState(() => ({ timeZone: findTimeZone(cityCountry) }));
 
     function tic() {
         setTime(new Date())
-    }   
-    
-    useEffect(() => {       
+    }
+
+    useEffect(() => {
         const interval = setInterval(tic, 1000);
         return () => clearInterval(interval);
     }, [])
@@ -30,6 +30,6 @@ export const Timer: React.FC<Props> = ({ cityCountry }) => {
 
 function findTimeZone(cityCountry: string): string | undefined {
     console.log("Enter func");
-    const res =  timeZones.findIndex((inp) => JSON.stringify(inp).includes(cityCountry));
+    const res = timeZones.findIndex((inp) => JSON.stringify(inp).includes(cityCountry));
     return res === -1 ? undefined : timeZones[res].name;
 }
