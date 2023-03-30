4bc2f18710d4c69cbc64fd1f9a3fe1367b85452d
AlGuron <blizgur8@gmail.com>
2023-03-30 19:32:04 +0300

    add HW#38

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Layout } from './components/navigators/Layout';
-import { Products } from './components/navigators/Products';
+import { Navigator } from './components/navigators/Navigator';
 import { Bread } from './components/pages/Bread';
 import { Customers } from './components/pages/Customers';
 import { Dairy } from './components/pages/Dairy';
 import { Home } from './components/pages/Home';
 import { NotFound } from './components/pages/NotFound';
 import { Orders } from './components/pages/Orders';
-
 import { ShoppingCart } from './components/pages/ShoppingCart';
+import navigators from './config/navigators.json'
 
 
 function App() {
-
+  const {layout, products} = navigators;
   return <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Layout />}>
+      <Route path='/' element={<Navigator linkInfo={layout}  />}>
         <Route index element={<Home />} />
         <Route path='customers' element={<Customers />} />
         <Route path='orders' element={<Orders />} />
         <Route path='shoppingcart' element={<ShoppingCart />} />
-        <Route path='products' element={<Products />}>
+        <Route path='products' element={<Navigator linkInfo={products}  />}>
           <Route path='dairy' element={<Dairy />} />
           <Route path='bread' element={<Bread />} />
         </Route>
---
+++ src/components/navigators/Navigator.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { NavLink, Outlet, useNavigate, useNavigation } from "react-router-dom"
+import './navigators.css'
+
+type Props = {
+    linkInfo: Array<{ itemLink: string, itemText: string }>,
+}
+
+export const Navigator: React.FC<Props> = ({ linkInfo }) => {
+    // const navigation = useNavigation();
+    // const navigate = useNavigate();
+    // let isReloading = navigation.state === "loading" &&
+    //     navigation.formData != null &&
+    //     navigation.formAction === navigation.location.pathname;
+
+    // useEffect( () =>{
+    //     navigate("/");
+    // } ,[isReloading]   )
+
+    function getLinks(linkInfo: { itemLink: string; itemText: string }[]): import("react").ReactNode {
+        const res = linkInfo.map((v, i) => <li className="navigator-item paragraph-l" key={i}>
+            <NavLink to={v.itemLink}>{v.itemText}</NavLink>
+        </li>);
+        return <ul className="navigator-list"> {res} </ul>
+    }
+
+    return <div>
+        <nav className="container">
+            {getLinks(linkInfo)}
+        </nav>
+        <Outlet />
+    </div>
+}
+
+
