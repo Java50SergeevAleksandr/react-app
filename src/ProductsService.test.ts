aaf883be10865ff4e51d866aa3b519d01b297699
AlGuron <blizgur8@gmail.com>
2023-04-20 22:36:22 +0300

    fix test

--- src/ProductsService.test.ts
+++ src/ProductsService.test.ts
@@ -2,23 +2,23 @@ import { productsService } from "./config/products-service-config"
 import productsConfig from "./config/products-config.json"
 import { getRandomNumber } from "./util/random";
 
-test("setProducts test", () => {
-    productsService.setProducts().then(count => {
-        expect(count).toEqual(productsConfig.length);
-    })
+test("setProducts test", async () => {
+    const count = await productsService.setProducts();
+    expect(count).toEqual(productsConfig.length);
 })
-test("category bread exists", () => {
-    productsService.isCategoryExist("bread")
-        .then(res => expect(res).toBeTruthy());
+test("category bread exists", async () => {
+    const res = await productsService.isCategoryExist("bread");
+    return expect(res).toBeTruthy();
 })
-test("category kukureku doesn't exist", () => {
-    productsService.isCategoryExist("kukureku")
-        .then(res => expect(res).toBeFalsy());
+test("category kukureku doesn't exist", async () => {
+   const res = await productsService.isCategoryExist("kukureku");
+    return expect(res).toBeFalsy();
 })
 
 // ===============HW#42===============
 
-test("Random category exists", () => {
+test("Random category exists", async () => {
+    jest.setTimeout(20000);
     const categories: string[] = productsConfig.map(pc =>
         pc.name.split("-")[0]).reduce((r: Array<string>, v) => {
             if (!r.includes(v)) {
@@ -28,23 +28,23 @@ test("Random category exists", () => {
         }, []);
 
     const categoryNumber = getRandomNumber(0, categories.length - 1);
-    productsService.isCategoryExist(categories[categoryNumber])
-        .then(res => expect(res).toBeTruthy());
+   const res = await productsService.isCategoryExist(categories[categoryNumber]);
+    return expect(res).toBeTruthy();
 })
 
-test("Remove category", () => {
-    productsService.removeCategory("bread")
-        .then(v => productsService.isCategoryExist("bread")
-            .then(res => expect(res).toBeFalsy()))
+test("Remove category", async () => {
+  const v = await productsService.removeCategory("bread");
+    const res = await productsService.isCategoryExist("bread");
+    return expect(res).toBeFalsy();
 })
 
-test("Add category", () => {
-    productsService.addCategory({ name: "bread" })
-        .then(v => productsService.isCategoryExist("bread")
-            .then(res => expect(res).toBeTruthy()))
+test("Add category", async () => {
+    const v = await productsService.addCategory({ name: "bread" });
+    const res = await productsService.isCategoryExist("bread");
+    return expect(res).toBeTruthy();
 })
 
-test("All categories exists", () => {
+test("All categories exists", async () => {
     const categories: string[] = productsConfig.map(pc =>
         pc.name.split("-")[0]).reduce((r: Array<string>, v) => {
             if (!r.includes(v)) {
@@ -53,7 +53,7 @@ test("All categories exists", () => {
             return r
         }, []);
 
-    Promise.all(categories.map((v) => productsService.isCategoryExist(v)))
-        .then(arP => arP.map(v => v))
-        .then(v => expect(v.every(b => b === true)).toBeTruthy());
+        const arP = await Promise.all(categories.map((v_1) => productsService.isCategoryExist(v_1)));
+    const v_3 = arP.map(v_2 => v_2);
+    return expect(v_3.every(b => b === true)).toBeTruthy();
 })
